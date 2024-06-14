b0778ee194f9b7e50952969b64f8c56c3eeaec36
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2024-06-14 17:09:41 -0300

    feat: create user profile section

---
+++ src/ui/sections/user-profile-section.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+import { User } from "@/domain/entities/user";
+
+type UserProfileSectionProps = {
+  user: User;
+};
+
+export function UserProfileSection({ user }: UserProfileSectionProps) {
+  return (
+    <aside className="flex flex-col items-center gap-2 border p-6 text-center">
+      <Image
+        src={user.imageUrl}
+        alt={`Imagem de perfil do usuário ${user.name}`}
+        width={200}
+        height={200}
+        className="rounded-full bg-grey-neutral"
+      />
+      <div>
+        <h2 className="text-xl font-semibold">{user.name}</h2>
+        <span>{user.identifier}</span>
+      </div>
+      <p>{user.description}</p>
+    </aside>
+  );
+}
