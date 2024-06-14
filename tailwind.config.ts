6f249dd1f4c9563c3d671af5f35b35876d7c705c
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2024-06-14 12:11:20 -0300

    chore: adjusts on tailwind style guide

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,24 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      padding: "2rem",
+    },
     extend: {
+      colors: {
+        primary: "#32c0c6",
+        "primary-dark": "#339599",
+        "primary-light": "#d4edef",
+        placeholder: "#8c8c8c",
+        "grey-neutral": "#616161",
+        "grey-dark": "#4e4e4e",
+        "white-background-light": "#ffffff",
+        "white-background-matte": "#f3f3f5",
+        "border-line": "#e4e6e9",
+        success: "#5db85b",
+        warning: "#ffb22c",
+        danger: "#fc4b6c",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
